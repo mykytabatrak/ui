f94b4f7ee161c594106f229dad6d171ebef27dd7
Mykyta Batrak <mbatrak@osdb.io>
2023-06-12 21:57:38 +0300

    feat(color-scheme): added color scheme switcher feat(css): added base color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: 'oklch(var(--white) / <alpha-value>)',
+      black: 'oklch(var(--black) / <alpha-value>)',
+
+      'app-background': 'oklch(var(--app-background) / <alpha-value>)',
+      'app-text': 'oklch(var(--app-text) / <alpha-value>)',
     },
   },
   plugins: [],
-}
+};
